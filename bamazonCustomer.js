3eecf7eb8a2c73d0f4b2a82a84a300250c209b5d
Apple User <appleuser@MacBook-Pro.local>
2017-11-23 15:01:34 -0500

    updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,26 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var Table = require('cli-table');
+//packages
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+// require('console.table');
 
+//database connection
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
   port: 8889,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "root",
-  database: "Bamazon"
+  user: 'root',
+  password: 'root',
+  database: 'bamazon'
 });
 
-connection.connect(function(err) {
+connection.connect(function(err){
   if (err) throw err;
 
   console.log("Connected to database.");
+  //call placeOrder function here
+  placeOrder();
+});
 
-  displayInventory();
+ displayInventory();
 
 // displayInventory will retrieve the current inventory from the database and output it to the console
 function displayInventory() {
@@ -52,11 +53,70 @@ function displayInventory() {
 
 }
 
-});
 
-     inquirer.prompt({
+//start variables
+
+var sellItem = "";
+var sellQty = 0;
+var totalPurchase = 0;
+var itemDept = "";
+
+var placeOrder = function() {
+    // query products table and display results using console.table
+    connection.query("SELECT item_id as 'Item', product_name as 'Name', department_name as 'Department', concat('$', format(price, 2)) as 'Price' from products", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        inquirer.prompt({
             name: "itemId",
             type: "input",
             message: "Enter the ID for the item you would like to purchase", //prompt user to id to buy
+        }).then(function(answer) {
+            sellItem = answer.itemId;
+            inquirer.prompt({
+                name: "qty",
+                type: "input",
+                message: "How many would you like?", // prompt user for quantity
+            }).then(function(answer) {
+                sellQty = answer.qty;
+                // query products table for quantity available for requested item and compare to quantity requested
+                connection.query("SELECT department_name, quantity FROM products WHERE ? ", [{ item_id: selItem }], function(err, res) {
+                    if (res[0].quantity < sellQty) { // not enough product
+                        console.log('');
+                        console.log('**************************');
+                        console.log('* Insufficient quantity! *');
+                        console.log('**************************');
+                        console.log('');
+                        placeOrder(); // start purchase process again
+                    } else { //
+                        itemDept = res[0].department_name; // determine dept name for requested item
+                        // update inventory for requested item
+                        connection.query("UPDATE products SET quantity = quantity - ? WHERE ?", [sellQty, { item_id: selItem }], function(err, res) {
+                            if (err) throw err;
+                            // get numeric value for total purchase
+                            connection.query("SELECT price * ? as 'tot_no_format' from products  WHERE ?", [sellQty, { item_id: selItem }], function(err, res) {
+                                if (err) throw err;
+                                totalPurchase = res[0].tot_no_format;
+                                // get formatted purchase total and display to console
+                                connection.query("SELECT concat('$', format(price * ?, 2)) as 'Total_Paid' from products  WHERE ?", [sellQty, { item_id: selItem }], function(err, res) {
+                                    if (err) throw err;
+                                    console.log('');
+                                    console.table(res);
+                                    // update sales in products table
+                                    connection.query("UPDATE products SET product_sales = product_sales + ? WHERE ?", [totalPurchase, { item_id: selItem }], function(err, res) {
+                                        if (err) throw err;
+                                        // update sales in departments table
+                                        connection.query("UPDATE departments SET total_sales = total_sales + ? WHERE ?", [totalPurchase, { department_name: itemDept }], function(err, res) {
+                                            if (err) throw err;
+                                            placeOrder(); // start purchase process again
+                                        });
+                                    });
+
+                                });
+                            });
+                        });
+                    }
+                });
             });
-     
+        });
+    });
+};
